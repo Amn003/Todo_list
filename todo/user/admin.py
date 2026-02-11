0acf5fd642399dfe8506843c8c50a5307b064818
Aman <aman757106@gmail.com>
2026-02-11 18:51:06 +0530

    a little change in the taske for once

--- todo/user/admin.py
+++ todo/user/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import profile ,User
+from .models import profile ,User,commnets
 # Register your models here.
 admin.site.register(profile)
+admin.site.register(commnets)
